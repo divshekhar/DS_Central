eb2b126b0f529ba21de96e76b78b423a6368024d
Divyanshu Shekhar <imdshekhar@gmail.com>
2023-07-21 17:35:12 +0530

    fix: CORS issue

--- Q1/src/index.ts
+++ Q1/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { authenticate } from "./auth/authentication";
 
 dotenv.config();
@@ -12,6 +13,9 @@ const PORT = process.env.PORT || 3000;
 
 // Middleware to parse JSON data
 app.use(express.json());
+
+// Enable CORS
+app.use(cors());
 
 // Home route to test the server
 app.get("/", authenticate, (req, res) => {
